b23f23d6a3f523bc97dc730c016b0ad3ca0856b3
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2026-02-11 12:54:13 +0700

    Fix typo PrismaExceptionFilter import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { PrismaExceptionilter } from './common/filters/prisma-exception.filter';
+import { PrismaExceptionFilter} from './common/filters/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
 
-  app.useGlobalFilters(new PrismaExceptionilter)
+  app.useGlobalFilters(new PrismaExceptionFilter)
   
   app.enableCors({
     origin: ['http://localhost:3000'], // frontend
